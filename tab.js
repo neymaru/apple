0ce634586e99b9f0fa4528734cb8bcea24a18cc8
neymaru <106804514+neymaru@users.noreply.github.com>
2023-02-12 23:15:48 +0900

    개인 공부  폴더 생성

---
+++ tab.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i < 3; i++) {
+//   $(".tab-button").eq(i).click(function () {
+//       탭열기(i);
+//     });
+// }
+
+function 탭열기(i) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(i).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(i).addClass("show");
+}
+
+$(".list").click(function (e) {
+  탭열기(e.target.dataset.id);
+});
